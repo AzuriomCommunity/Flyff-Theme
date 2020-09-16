9a77b573cecc9616da8293f615c0b1df3cbf9aa7
Javdu10 <jav.valentin@gmail.com>
2020-09-16 02:19:52 +0200

    set session vars m_idPlayer & m_nServer

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -1,6 +1,10 @@
 @if (request()->is_game || session()->has('azuriom_is_game'))
 @php
-    session()->put('azuriom_is_game','1');
+    session('azuriom_is_game','1');
+    if(!session()->has('m_idPlayer')) {
+        session(['m_idPlayer' => request()->m_idPlayer]);
+        session(['m_nServer'=> request()->m_nServer]);
+    }
 @endphp
     
     @include('layouts.game')
